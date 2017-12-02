71d1884408c8eca8490d021e1ad5101133a3d4e8
Radoslav Radoev <rradoev@gmail.com>
2017-12-02 00:03:43 -0800

    Las Vegas Dice app

---
+++ VarConstDataType/MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 31
+
+print(myAge)
+
+myAge = 32
+
+print(myAge)
+
+let myName : String = "Rado"
+let mySurname : String = "Radoev"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) \(mySurname)"
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumebrs : Int = 12
+let text : String = "cactus"
+
+let booleans : Bool = true
+
+let floating : Float = 12.2
+myAge.
