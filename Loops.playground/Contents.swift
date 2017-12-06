33b666ee19df86ad65efe5efe6b40d7aa870d362
Radoslav Radoev <rradoev@gmail.com>
2017-12-05 22:54:33 -0800

    A test playgrounds

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -6,9 +6,9 @@ func beerSong(_ totalNumberOfBottles : Int) -> String {
     var bottle : String = ""
     
     for number in (1...totalNumberOfBottles).reversed() {
-        if number - 1 == 0 {bottle = "no more"} else if number == 1 {bottle = "bottle"} else { bottle = "bottles"}
+        if number == 1 {bottle = "bottle"} else { bottle = "bottles"}; if number - 1 == 0 {bottle = "no more"}
         lyrics += "\(number) " + bottle + " of beer on the wall, \(number) bottles of beer\n"
-        lyrics += "Take one down and pass it around, \(bottle) bottles of beer\n\n"
+        lyrics += "Take one down and pass it around, \(number - 1 == 0 ? "no more" : \(number)) bottles of beer\n\n"
     }
     
     lyrics += "No more bottles of beef on the wall, no more bottles of beer.\n" +
