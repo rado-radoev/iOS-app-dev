270f13ca91ba5163ff06f918ce77fc0103deed06
Radoslav Radoev <rradoev@gmail.com>
2017-12-09 01:33:25 -0800

    Question app

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var selectedQuestion : Question?
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +22,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        selectedQuestion = allQuestions.list[Int(arc4random_uniform(UInt32(allQuestions.list.count)))]
+        questionLabel.text = selectedQuestion?.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
     }
     
     
@@ -40,7 +50,12 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
-        
+        if selectedQuestion?.answer == pickedAnswer {
+            print("You got it!")
+        }
+        else {
+            print ("You are wrong!")
+        }
     }
     
     
