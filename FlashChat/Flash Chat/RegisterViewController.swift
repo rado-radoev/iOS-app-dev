782be72db082e9ffa1dc0a414fb70ce8b13d2afc
Radoslav Radoev <rradoev@gmail.com>
2018-01-09 00:43:42 -0800

    Back on the Flash Chat app

--- FlashChat/Flash Chat/RegisterViewController.swift
+++ FlashChat/Flash Chat/RegisterViewController.swift
@@ -29,8 +29,7 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
-        
+       
         //TODO: Set up a new user on our Firbase database
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
             (user, error) in
