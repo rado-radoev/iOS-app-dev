0d5c87e14cc71bb26fb9e34b560a4a3b610551e4
Radoslav Radoev <rradoev@gmail.com>
2018-01-14 22:59:27 -0800

    ti si maloumene za kakvo ti e ios programirane?? Ti JAVA-ta nauchi li q ta shte mi progrmairash na ios

---
+++ FirebaseTest/FirebaseTest/LoginController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginController.swift
+//  FirebaseTest
+//
+//  Created by Radoslav Radoev on 1/14/18.
+//  Copyright © 2018 Radoslav Radoev. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class LoginController : UIViewController {
+    
+}
---
+++ FirebaseTest/FirebaseTest/PictureDisplayController.swift
@@ -0,0 +1,13 @@
+//
+//  PictureDisplayController.swift
+//  FirebaseTest
+//
+//  Created by Radoslav Radoev on 1/14/18.
+//  Copyright © 2018 Radoslav Radoev. All rights reserved.
+//
+
+import UIKit
+
+class PictureDisplayController: UIViewController {
+    
+}
